3127519e60ab00802d47ec489088b5df73c92f0f
EduBonfim <134088977+EduBonfim@users.noreply.github.com>
2024-10-17 20:00:48 -0300

    Add files via upload

---
+++ Somatorio.java
@@ -0,0 +1,16 @@
+public class Somatorio{
+
+    public static void main(String[] args){
+
+        int contador= 0;
+        int acumulador= 0;
+
+        while (contador <=100){
+            acumulador = acumulador+contador;
+            contador ++;
+        }
+    
+    System.out.println("A soma é  "+acumulador);
+    }
+
+}
